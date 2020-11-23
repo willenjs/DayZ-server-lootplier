b747e5ef967db5a93071d1e98b847537f9500339
Willen <willenjs@gmail.com>
2020-11-23 00:10:56 -0300

    Cleaning the index.js.

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-var convert = require('xml-js');
+const reader = require("readline-sync");
 const fs = require('fs');
-var options = {compact: true, ignoreComment: true, spaces: 4};
 
-// this example reads the file synchronously
-// you can read it asynchronously also
-let xml_string = fs.readFileSync("./bkp/types.xml", "utf8");
+const options = {compact: true, ignoreComment: true, spaces: 4};
 
-var resultJson = convert.xml2json(xml_string, options);
-console.dir(resultJson);
+let lootTimes = 1;
 
-var resultXml = convert.json2xml(resultJson, options);
-console.log(resultXml);
-
-fs.writeFileSync('./lastedit/types.xml', resultXml)
+lootTimes = reader.question("I want loot to be increased by X times: ");
